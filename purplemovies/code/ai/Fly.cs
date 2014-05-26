1e9a5bce2e16e5baee00f82764b6ec4c5bdc9969
Ash Rawstron <ash.rawstron@gmail.com>
2014-05-25 17:17:58 -0700

    Increased fly-speed for shells.

--- purplemovies/code/ai/Fly.cs
+++ purplemovies/code/ai/Fly.cs
@@ -1,19 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Fly : MonoBehaviour {
-
-	// Use this for initialization
+public class Fly : MonoBehaviour
+{
 	void Start ()
 	{
 //		rigidbody.AddRelativeForce (new Vector3( 0f, 0f, 1000f ));
-		rigidbody.AddRelativeForce (new Vector3( 0f, 3000f, 0f ));
+		rigidbody.AddRelativeForce (new Vector3( 0f, 30000f, 0f ));
 //		rigidbody.AddRelativeForce (new Vector3( 1000f, 0f, 0f ));
 //		rigidbody.velocity = new Vector3( 0f, 0f, 1000f);
 	}
-	
-//	// Update is called once per frame
-//	void Update () {
-//	
-//	}
 }
